PavelJurasek/PHPWander

commit ef3e220414d55f16412010b3c2e8150aacefebbf

Simplify assignment of temporaries in Scope

src/Analyser/Scope.php
@@ -8,6 +8,7 @@ use PHPCfg\Op\Expr;
 use PHPCfg\Op\Expr\FuncCall;
 use PHPCfg\Op\Expr\NsFuncCall;
 use PHPCfg\Op\Stmt;
+use PHPCfg\Operand;
 use PHPCfg\Operand\Temporary;
 use PHPWander\Taint;
 use PHPWander\TransitionFunction;
@@ -280,8 +281,12 @@ class Scope
 		return isset($this->temporaries[spl_object_hash($temporary)]);
 	}
 
-	public function assignTemporary(Temporary $temporary, int $taint = Taint::UNKNOWN): self
+	public function assignTemporary(Operand $temporary, int $taint = Taint::UNKNOWN): self
 	{
+		if (!$temporary instanceof Temporary) {
+			return $this;
+		}
+
 		$temporaryTaints = $this->getTemporaryTaints();
 		$temporaryTaints[$this->hash($temporary)] = $taint;
 

src/Analyser/NodeScopeResolver.php
@@ -251,13 +251,8 @@ class NodeScopeResolver
 		$op->setAttribute(Taint::ATTR, $taint);
 		$scope = $scope->assignVariable($name, $taint);
 
-		if ($op->expr instanceof Operand\Temporary) {
-			$scope = $scope->assignTemporary($op->expr, $taint);
-		}
-
-		if ($op->result instanceof Operand\Temporary) {
-			$scope = $scope->assignTemporary($op->result, $taint);
-		}
+		$scope = $scope->assignTemporary($op->expr, $taint);
+		$scope = $scope->assignTemporary($op->result, $taint);
 //		taint($scope->getVariableTaints());
 
 		return $scope;
@@ -278,9 +273,7 @@ class NodeScopeResolver
 					$op->setAttribute(Taint::ATTR, $taint);
 				}
 
-				if ($op->result instanceof Operand\Temporary) {
-					$scope = $scope->assignTemporary($op->result, $taint);
-				}
+				$scope = $scope->assignTemporary($op->result, $taint);
 //				$op->result->setAttribute(Taint::ATTR, $taint);
 			} else {
 				dump(__METHOD__);

commit 9371d2d2702d071c6472c0db3830cedf4723eeb2

Scope::leaveBlock: fix returning result taint, broken by ef3e220

src/Analyser/Scope.php
@@ -166,7 +166,10 @@ class Scope
 
 	public function leaveBlock(): self
 	{
-		return $this->parentScope;
+		$parentScope = $this->parentScope;
+		$parentScope->setResultTaint($this->getResultTaint());
+
+		return $parentScope;
 	}
 
 	public function getStatementForBlock(Block $block): ?Stmt